42ef69b606bed42dd64055c2041427d18ef91b91
podum <patrick.odum1@gmail.com>
2022-07-01 02:43:35 +0000

    stripe

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import Stripe from 'stripe';
-const stripe = new Stripe('sk_test_...', {
+const stripe = new Stripe(functions.config().stripe.key.test, {
   apiVersion: '2020-08-27',
 });
 
@@ -8,11 +8,34 @@ const stripe = new Stripe('sk_test_...', {
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const refund = functions.https.onRequest(async (data: any, context) => {
-  const refund = await stripe.refunds.create({
-    charge: data.chargeId,
-    refund_application_fee: true,
-  }, {
-    stripeAccount: data.stripeId,  
-  });
-  functions.logger.info(refund);
+    const refund = await stripe.refunds.create({
+        charge: data.chargeId,
+        refund_application_fee: true,
+    }, {
+        stripeAccount: data.stripeId,  
+    });
+    functions.logger.info(refund);
+});
+
+export const subscriptionCancel = functions.https.onCall(async (data: any, context) => {
+    const cancel = await stripe.subscriptions.del(data.subscriptionId);
+    functions.logger.info(cancel);
+});
+
+export const subscriptionEnd = functions.https.onCall(async (data: any, context) => {
+    const end = await stripe.subscriptions.del(data.subscriptionId);
+    functions.logger.info(end);
+});
+
+export const subscriptionRetrieve = functions.https.onCall(async (data: any, context) => {
+    const subscription = await stripe.subscriptions.retrieve(data.subscriptionId);
+    stripe.subscriptions.update(data.subscriptionId, {
+        cancel_at_period_end: false,
+        proration_behavior: 'create_prorations',
+        items: [{
+            id: subscription.items.data[0].id,
+            // price: data.priceId,
+        }]
+    });
+    functions.logger.info(subscription);
 });
